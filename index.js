bdaf7f6f726dd26a2b0fe313564697ee17d6cc41
Silvan Buedenbender <silvan@claiv.de>
2021-03-20 15:50:57 +0100

    Add generation of tags

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import toml from '@iarna/toml';
-import fs from 'fs';
-import {Octokit} from "@octokit/core";
+import toml from "@iarna/toml";
+import fs from "fs";
+import { Octokit } from "@octokit/core";
 
 const token = process.env.TOKEN;
 const github = new Octokit({
@@ -8,35 +8,46 @@ const github = new Octokit({
 });
 
 async function getStats(owner, repo) {
-  const {data} = await github.request(
-    'GET /repos/{owner}/{repo}/stats/code_frequency',
-    {owner, repo}
+  const { data } = await github.request(
+    "GET /repos/{owner}/{repo}/stats/code_frequency",
+    {
+      owner,
+      repo,
+    }
   );
   return data;
 }
 
 async function getRepoInfo(owner, repo) {
-  const {data} = await github.request(
-    'GET /repos/{owner}/{repo}',
-    {owner, repo}
-  );
+  const { data } = await github.request("GET /repos/{owner}/{repo}", {
+    owner,
+    repo,
+  });
   return data;
 }
 
 async function main() {
-  const file = 'data/github.toml'
-  const outputFolder = 'build';
+  const file = "data/github.toml";
+  const outputFolder = "build";
 
-  const raw = fs.readFileSync(file, 'utf8')
-  const {config, links} = toml.parse(raw)
+  const raw = fs.readFileSync(file, "utf8");
+  const { config, links } = toml.parse(raw);
 
+  const tags = Array.from(
+    Object.values(links).reduce(
+      (set, tags) => (tags.map((t) => set.add(t)), set),
+      new Set()
+    )
+  );
 
-  const data = Object
-    .entries(links)
-    .reduce((obj, [ident, tags]) => ({...obj, [config.prefix + ident]: tags}), {})
+  const goms = Object.entries(links).reduce(
+    (obj, [ident, tags]) => ({ ...obj, [config.prefix + ident]: tags }),
+    {}
+  );
 
-  if (!fs.existsSync(outputFolder)) fs.mkdirSync(outputFolder)
-  fs.writeFileSync(`${outputFolder}/data.json`, JSON.stringify(data))
+  if (!fs.existsSync(outputFolder)) fs.mkdirSync(outputFolder);
+  fs.writeFileSync(`${outputFolder}/goms.json`, JSON.stringify(goms));
+  fs.writeFileSync(`${outputFolder}/tags.json`, JSON.stringify(tags));
 }
 
 main();
